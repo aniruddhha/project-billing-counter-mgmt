9218eba3fdd522054fbec009dad9dc41b1dd03cd
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-10-12 12:37:14 +0530

    create customer and landing page

--- bcm-yt/app/routes/dashboard.customers.tsx
+++ bcm-yt/app/routes/dashboard.customers.tsx
@@ -1,7 +1,47 @@
+import { Link } from '@remix-run/react'
+import {del, pencil } from '../icons'
+
 export default function Customers() {
+
     return (
-        <>
-            <h1> This is customers landing page </h1>
-        </>
+        <section className="container h-[100%]">
+            <div className="w-[80%] flex justify-between items-center mt-5 ml-5">
+                <h1 className="text-4xl">Customers</h1>
+                <div>
+                    <span><Link to='../newcustomer'><u>New</u></Link></span>
+                    <input type="text" className="ml-3 bg-slate-100 h-8 p-2 border" placeholder='Mobile' />
+                </div>
+            </div>
+            <div className='mt-5 ml-5 w-[80%]'>
+                <table className='table-fixed w-[100%] shadow-lg bg-white'>
+                    <thead>
+                        <tr className='h-10'>
+                            <th className='border border-slate-300 w-[5%]'>Sr</th>
+                            <th className='border border-slate-300 w-[20%] text-left'><span className='ml-3'>Mobile</span></th>
+                            <th className='border border-slate-300 w-[25%] text-left ml-3'><span className='ml-3'>Name</span></th>
+                            <th className='border border-slate-300 w-[25%] text-left ml-3'><span className='ml-3'>Email</span></th>
+                            <th className='border border-slate-300'>Dob</th>
+                            <th className='border border-slate-300'>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className='h-10'>
+                            <td className='border border-slate-300 text-center'>1</td>
+                            <td className='border border-slate-300 ml-3'><span className='ml-3'>9089786756</span></td>
+                            <td className='border border-slate-300 ml-3'><span className='ml-3'>Abc</span></td>
+                            <td className='border border-slate-300 ml-3'><span className='ml-3'>abc@dd.com</span></td>
+                            <td className='border border-slate-300 text-center'>2000-01-01</td>
+                            <td className='border border-slate-300 text-center'>
+                                <div className='flex justify-around items-center'>
+                                    <span className='text-blue-700'>{pencil}</span>
+                                    <span className='text-red-700'>{del}</span>
+                                </div>
+                            </td>
+                        </tr>
+                        
+                    </tbody>
+                </table>
+            </div>
+        </section>
     )
 }
--- bcm-yt/app/routes/dashboard.newcustomer.tsx
+++ bcm-yt/app/routes/dashboard.newcustomer.tsx
@@ -1,7 +1,33 @@
 export default function NewCustomer() {
     return (
-        <>
-            <h1> Creating new customer </h1>
-        </>
+        <section className="container h-[100%]">
+            <div className="ml-5 mt-5">
+                <h1 className="text-4xl"> New Customer </h1>
+            </div>
+            <div className="w-[30%] ml-5 shadow-lg p-5 bg-slate-100 mt-5">
+                <form>
+                    <div className="flex flex-col mt-3">
+                        <label htmlFor="mobile" className="text-gray-500">Mobile</label>
+                        <input type="text" id="mobile" className="px-4 py-2 border" placeholder="Mobile" />
+                    </div>
+                    <div className="flex flex-col mt-3">
+                        <label htmlFor="name" className="text-gray-500">Name</label>
+                        <input type="text" id="name" className="px-4 py-2 border" placeholder="Name" />
+                    </div>
+                    <div className="flex flex-col mt-3">
+                        <label htmlFor="email" className="text-gray-500">Email</label>
+                        <input type="email" id="email" className="px-4 py-2 border" placeholder="Email" />
+                    </div>
+                    <div className="flex flex-col mt-3">
+                        <label htmlFor="dob" className="text-gray-500">Dob</label>
+                        <input type="date" id="dob" className="px-4 py-2 border" placeholder="Dob" />
+                    </div>
+                    <div className="flex justify-center mt-3">
+                        <input type="submit" value="Create" className="bg-lime-600 p-2 text-white w-20 hover:bg-lime-700 active:bg-lime-800 rounded-full" />
+                    </div>
+                </form>
+            </div>
+
+        </section>
     )
 }
