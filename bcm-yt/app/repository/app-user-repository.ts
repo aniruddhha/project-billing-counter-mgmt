b33ae6dfe6d36361051947fcfa757377171d466b
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-11-10 19:07:24 +0530

    user : create app user

--- bcm-yt/app/repository/app-user-repository.ts
+++ bcm-yt/app/repository/app-user-repository.ts
@@ -9,4 +9,9 @@ export class AppUserRepository implements IUserRepository {
     async login(user: IAppUser): Promise<IAppUser | null | undefined> {
         return AppUser.findOne(user).then( usr => usr?.toObject())
     }
+
+    async create(user: IAppUser): Promise<IAppUser> {
+        const us = new AppUser(user)
+        return us.save().then(u => us.toObject())
+    }
 }
